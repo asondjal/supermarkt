469dcff8cad45aacb37cf66890d08678dcd7118c
Arrif.Sondjalim <arrif.sondjalim@hm.edu>
2025-07-07 22:52:56 +0200

    Nothing important was changed

--- python_tests/test_statistik.py
+++ python_tests/test_statistik.py
@@ -15,7 +15,7 @@ def test_statistik():
     assert statistik.wortanzahl() == 14
     assert statistik.zaehle_variable("Produkt") == 1
     assert statistik.zaehle_eintraege() == 1
-    assert statistik.zeichenanzahl() == 598
+    assert statistik.zeichenanzahl() == 596
     statistik.lade_daten(kunden_data)
     assert statistik.zaehle_linen() == 22
     assert statistik.wortanzahl() == 38
