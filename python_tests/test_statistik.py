960597c07535c926da6053f2f0a8d03d3a1b4527
Arrif.Sondjalim <arrif.sondjalim@hm.edu>
2025-09-08 21:05:10 +0200

    Minor changes for CI/CD

--- python_tests/test_statistik.py
+++ python_tests/test_statistik.py
@@ -12,13 +12,13 @@ def test_statistik():
     statistik.entferne_daten(text.read_txt())
     statistik.lade_daten(text)
     assert statistik.zaehle_linen() == 11
-    # assert statistik.wortanzahl() == 14
+    assert statistik.wortanzahl() == 14
     assert statistik.zaehle_variable("Produkt") == 1
     assert statistik.zaehle_eintraege() == 1
     assert statistik.zeichenanzahl() == 598
     statistik.lade_daten(kunden_data)
     assert statistik.zaehle_linen() == 22
-    # assert statistik.wortanzahl() == 38
+    assert statistik.wortanzahl() == 38
     assert statistik.zaehle_variable("Kunde") == 1
     assert statistik.zaehle_eintraege() == 2
     assert 7.508571147 - statistik.mittelwert() < 0.0001
