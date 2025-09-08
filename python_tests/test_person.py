8ba8d5f72830015a3887d8a882162394426fe555
Arrif.Sondjalim <arrif.sondjalim@hm.edu>
2025-09-08 17:02:41 +0200

    Modified tests for determining the correct IDs for the classes Konto and Person

--- python_tests/test_person.py
+++ python_tests/test_person.py
@@ -10,15 +10,14 @@ def test_person():
     person2 = Person(
         "Magic Johnson", "male", 64, "magic.johnson@yahoo.hotmail", "Los Angeles, USA"
     )
-
+    assert person1.get_person_id() != person2.get_person_id()
+    assert person2.get_person_id() > person1.get_person_id()
     assert person1.get_name() == "LeBron James"
     assert person1.get_geschlecht() == "male"
     assert person1.get_alter() == 40
     assert person1.get_email() == "lebron.james@gmail.com"
     assert person1.get_adresse() == "Los Angeles, USA"
     assert person1.vergleiche_person(person2) is False
-    assert person1.get_person_id() == 0
-    assert person2.get_person_id() == 1
     assert str(person1) == f"Person: {person1.get_name()}"
 
 test_person()
